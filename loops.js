345d7c1ee3422c70a2911d61c688802c87d95611
Ksena <oksishukh@yahoo.com>
2017-12-17 10:32:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
 let n = 0;
 function whileLoop(n) {
- while(n>0) {
-   console.log(--n)
- }
- return 'done'
+  while (n > 0) {
+    console.log(--n);
+  }
+  return "done";
 }
+
 
 function doWhileLoop(array) {
 do {
