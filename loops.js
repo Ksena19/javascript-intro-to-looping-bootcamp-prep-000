bf9f605a1d64fa669b6314639925e5f97b251f24
Ksena <oksishukh@yahoo.com>
2017-12-17 10:34:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,19 +7,13 @@ function whileLoop(n) {
 }
 
 
-whileLoop(10);*/
-
 function maybeTrue() {
-  return Math.random() >= 0.5;
-}
-
-var array = [1,2,3];
-
-function doWhileLoop(array) {
-  do {
-    array.pop();
-  } while (array.length > 0); //&& maybeTrue());
-  return array;
-}
-
-doWhileLoop(array);
+   return Math.random() >= 0.5
+ }
+ 
+ function doWhileLoop(array) {
+    do {
+      array.pop();
+   } while (array.length > 0 && maybeTrue());
+   return array;
+ } 
